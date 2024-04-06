1167a796feb4abeb1181d267ba2ef4708b6306b6
Artur Thurkan <otcheola2015@gmail.com>
2024-04-06 23:21:38 +0300

    Izmeneie

---
+++ Domashka/Program.cs
@@ -0,0 +1,22 @@
+﻿
+ 
+ 
+    
+    {
+        Console.Write("Введите значение M: "); 
+        int.TryParse(Console.ReadLine(), out int M);
+ 
+        Console.Write("Введите значение N: "); 
+        int.TryParse(Console.ReadLine(), out int N);
+    
+        PrintNaturalNumbers(M, N); 
+    }
+ 
+    static void PrintNaturalNumbers(int current, int N)
+    {
+        if (current <= N) 
+        { 
+            Console.Write(current + " "); 
+            PrintNaturalNumbers(current + 1, N); 
+        } 
+    }
