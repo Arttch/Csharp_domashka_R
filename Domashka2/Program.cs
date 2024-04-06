99314c21f66434005c2c494610c44460fb83b41b
Artur Thurkan <otcheola2015@gmail.com>
2024-04-06 23:22:24 +0300

    Izmeneie2

---
+++ Domashka2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+ 
+
+
+
+    
+        int[] myArray = { 10, 20, 30, 40, 50 }; 
+ 
+        Console.WriteLine("Элементы массива, начиная с конца:"); 
+        PrintArrayReverse(myArray, myArray.Length - 1); 
+    
+ 
+     void PrintArrayReverse(int[]  myArray, int index) 
+    { 
+        if (index >= 0) 
+        { 
+            Console.Write(myArray[index] + " "); 
+            PrintArrayReverse(myArray, index - 1); 
+        } 
+    } 
